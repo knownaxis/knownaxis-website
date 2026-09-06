a9dd73631425eb0807d17d589b3388a342e13db0
Vizbix <vizbixhq@gmail.com>
2026-09-06 21:36:52 +0530

    Fix basePath for Cloudflare

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
-// If deploying to GitHub Pages under https://username.github.io/REPO_NAME,
-// set basePath/assetPrefix to '/REPO_NAME'. Leave empty for Vercel or a custom domain.
-const repoName = '/knownaxis'; // <-- change to your repo name, or set to '' if not needed
-
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  basePath: isProd ? repoName : '',
-  assetPrefix: isProd ? repoName : '',
   trailingSlash: true,
 };
 
